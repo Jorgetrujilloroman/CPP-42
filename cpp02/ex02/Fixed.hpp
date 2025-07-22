e70da6fab16fe8fa772b6ce727318f9320492854
Jorge Trujillo Roman <jotrujil@c3r7s6.42malaga.com>
2025-07-22 18:54:13 +0200

    adding phony to all makefiles. TODO: CPP02 ex02

---
+++ cpp02/ex02/Fixed.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jotrujil <jotrujil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/03 16:45:55 by jotrujil          #+#    #+#             */
+/*   Updated: 2025/07/22 18:42:28 by jotrujil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+class Fixed {
+private:
+	int	_value;
+	static const int _fractionalBits = 8;
+public:
+	Fixed();
+	Fixed(const Fixed& copy);
+	Fixed(const int intNum);
+	Fixed(const float floatNum);
+	Fixed& operator = (const Fixed& copy);
+	~Fixed();
+
+	int		getRawBits( void ) const;
+	void	setRawBits( int const raw );
+	float	toFloat( void ) const;
+	int		toInt( void ) const;
+};
+
+std::ostream&	operator<<(std::ostream& os, const Fixed& fixed);
+
+#endif
---
+++ cpp02/ex02/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jotrujil <jotrujil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/22 18:49:56 by jotrujil          #+#    #+#             */
+/*   Updated: 2025/07/22 18:50:17 by jotrujil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main( void ) {
+Fixed a;
+Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+std::cout << a << std::endl;
+std::cout << ++a << std::endl;
+std::cout << a << std::endl;
+std::cout << a++ << std::endl;
+std::cout << a << std::endl;
+std::cout << b << std::endl;
+std::cout << Fixed::max( a, b ) << std::endl;
+return 0;
+}
