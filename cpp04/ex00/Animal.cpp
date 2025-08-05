51dde9fbfe1157372c37caf9a0ef17082ac702d3
Jorge Trujillo Roman <jotrujil@c3r8s4.42malaga.com>
2025-08-05 20:01:34 +0200

    starting ex00 from cpp04

---
+++ cpp04/ex00/Animal.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Animal.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jotrujil <jotrujil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 19:40:12 by jotrujil          #+#    #+#             */
+/*   Updated: 2025/08/05 20:00:33 by jotrujil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp"
+
+Animal::Animal() : type("Animal") {
+	std::cout << "A generic animal has appeared!" << std::endl;
+}
+
+Animal::Animal(const std::string& animalType) : type(animalType) {
+	std::cout << "A new animal type: " << type << " has appeared!";
+}
+
+Animal::~Animal() {
+	std::cout << "This animal was destroyed... (No animals were injured)" << std::endl;
+}
+
+Animal::Animal(const Animal& copy) {
+	type = copy.type;
+		std::cout << "Copy constructor called for type: " << type << std::endl;
+}
+
+Animal& Animal::operator= (const Animal& copy) {
+	if(this != &copy) {
+		
+	}
+}
+
+// Functions
+
+std::string		Animal::getType() const {
+
+}
+
+void	Animal::makeSound() const {
+
+}
---
+++ cpp04/ex00/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jotrujil <jotrujil@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/05 19:23:34 by jotrujil          #+#    #+#             */
+/*   Updated: 2025/08/05 19:29:26 by jotrujil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Animal.hpp" 
+
+int main()
+{
+const Animal* meta = new Animal();
+const Animal* j = new Dog();
+const Animal* i = new Cat();
+std::cout << j->getType() << " " << std::endl;
+std::cout << i->getType() << " " << std::endl;
+i->makeSound(); //will output the cat sound!
+j->makeSound();
+meta->makeSound();
+...
+return 0;
+}
