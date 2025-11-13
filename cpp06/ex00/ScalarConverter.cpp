751d96f85b696d29eee1453c84091871a0318750
Jorge <jorgetrujilloroman@gmail.com>
2025-11-13 17:55:55 +0100

    finishing polemical tests

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <limits>
 #include <cctype>
+#include <cmath>
+#include <iomanip>
 
 using std::cout;
 using std::endl;
@@ -18,15 +20,23 @@ ScalarConverter& ScalarConverter::operator= (const ScalarConverter& copy) {
 ScalarConverter::~ScalarConverter() {}
 
 void	ScalarConverter::convert(const std::string& literal) {
+	
+	cout << std::setprecision(10);
 
 	double	value;
-	char*	endptr;
+	char*	endptr = NULL;
+	bool	is_char_literal = false;
 	bool	is_pseudo_literal = false;
 
-
 	// Identify the type
-	if (literal.length() == 3 && (literal[0] == '\'') && (literal[2] == '\'')) {
-		value = static_cast<double>(literal[1]);
+	if ((literal.length() == 3 && (literal[0] == '\'') && (literal[2] == '\'')) ||
+		(literal.length() == 1 && !std::isdigit(literal[0]) && literal[0] != '.' &&
+		literal[0] != '+' && literal[0] != '-') ) {
+		if (literal.length() == 3)
+			value = static_cast<double>(literal[1]);
+		else if (literal.length() == 1)
+			value = static_cast<double>(literal[0]);
+		is_char_literal = true;
 	}
 	else if (literal == "nan" || literal == "+inf" || literal == "-inf" ||
 		 literal == "nanf" || literal == "+inff" || literal == "-inff") {
@@ -54,5 +64,49 @@ void	ScalarConverter::convert(const std::string& literal) {
 			cout << "Non-displayable" << endl;
 	}
 
-	
+	cout << "int: ";
+	if (is_pseudo_literal || value < static_cast<double>(std::numeric_limits<int>::min()) || 
+		value > static_cast<double>(std::numeric_limits<int>::max()) || std::isnan(value) ||
+		 std::isinf(value))
+		cout << "impossible" << endl;
+	else
+		cout << static_cast<int>(value) << endl;
+
+	cout << "float: ";
+	if (is_pseudo_literal) {
+		if (std::isnan(value))
+			cout << "nanf" << endl;
+		else if (std::isinf(value)) {
+			if (value > 0)
+				cout << "+inff" << endl;
+			else
+				cout << "-inff" << endl;
+		}
+	}
+	else {
+		float f = static_cast<float>(value);
+
+		if (f == static_cast<int>(f) && !is_char_literal)
+			cout << f << ".0f" << endl;
+		else 
+			cout << f << "f" << endl;
+	}
+
+	cout << "double: ";
+	if (is_pseudo_literal) {
+		if (std::isnan(value))
+			cout << "nan" << endl;
+		else if (std::isinf(value)) {
+			if (value > 0)
+				cout << "+inf" << endl;
+			else
+				cout << "-inf" << endl;
+		}
+	}
+	else {
+		if (value == static_cast<int>(value) && !is_char_literal)
+			cout << value << ".0" << endl;
+		else 
+			cout << value << endl;
+	}
 }
--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jotrujil <jotrujil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/13 13:11:04 by jotrujil          #+#    #+#             */
-/*   Updated: 2025/11/13 13:16:13 by jotrujil         ###   ########.fr       */
+/*   Updated: 2025/11/13 17:19:02 by jotrujil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +21,26 @@ int	main(int argc, char **argv) {
 	ScalarConverter::convert(argv[1]);
 	return 0;
 }
+
+/* 
+
+Polemic tests:
+
+./convert 2147483648
+./convert -2147483649
+./convert 1.8e308
+./convert 300
+./convert nanf
+./convert -inf
+./convert +inff
+./convert 99.99999
+./convert -0.5
+./convert -0.0f
+./convert '@'
+./convert 7
+./convert 32
+./convert 42a
+./convert 42f.0
+./convert 1.0.0 
+
+*/
