5393b55aff8dcd0227c481f1d226fe8bf308ef39
Jorge Trujillo Roman <jotrujil@c3r5s1.42malaga.com>
2025-11-18 19:33:07 +0100

    Upload ex00

---
+++ cpp07/ex00/whatever.hpp
@@ -0,0 +1,16 @@
+template <typename T>
+void	swap(T &a, T &b) {
+	T temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+const T	min(T &a, T &b) {
+	return (a <= b ? a : b);
+}
+
+template <typename T>
+const T	max(T &a, T &b) {
+	return (a >= b ? a : b);
+}
